8f63816daab2d98194da156f529b6232e3932c4e
BiggieFrost <hardysnoah@icloud.com>
2023-01-20 16:05:27 -0800

    Completely changed the template. From default Electron-Quick-Start, to my own template

--- src/index.js
+++ src/index.js
@@ -1,36 +1,148 @@
-// include the Node.js 'path' module at the top of your file
-const {app, BrowserWindow} = require("electron")
-const path = require('path')
+// index.js
 
-// modify your existing createWindow() function
+// constants
+const { app, BrowserWindow, Menu } = require('electron')
+
+// create a window
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hiddenInset',
+    title: 'Molecule',
     webPreferences: {
-      // preload: path.join(__dirname, 'preload.js')
       nodeIntegration: true
     }
   })
 
   win.loadFile('src/index.html')
-
-  // load developer tools
-  if(process.env.NODE_ENV === 'developer')
-    win.webContents.openDevTools()
+  if (process.env.NODE_ENV === 'developer') { win.webContents.openDevTools() }
 }
 
-// when the app is loaded and ready
+// when the app is ready and loaded
 app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
-// when the all open windows are closed
+// when all windows are closed
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') 
-        app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
+
+// app menu
+const isMac = process.platform === 'darwin'
+const isDevEnv = process.env.NODE_ENV === 'developer'
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac
+    ? [{
+        label: app.name,
+        submenu: [
+          { role: 'about' },
+          { type: 'separator' },
+          { role: 'services' },
+          { type: 'separator' },
+          { role: 'hide' },
+          { role: 'hideOthers' },
+          { role: 'unhide' },
+          { type: 'separator' },
+          { role: 'quit' }
+        ]
+      }]
+    : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac
+        ? [
+            { role: 'pasteAndMatchStyle' },
+            { role: 'delete' },
+            { role: 'selectAll' },
+            { type: 'separator' },
+            {
+              label: 'Speech',
+              submenu: [
+                { role: 'startSpeaking' },
+                { role: 'stopSpeaking' }
+              ]
+            }
+          ]
+        : [
+            { role: 'delete' },
+            { type: 'separator' },
+            { role: 'selectAll' }
+          ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      ...(isDevEnv
+        ? [{ role: 'toggleDevTools' }
+          ]
+        : [
+
+          ]),
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac
+        ? [
+            { type: 'separator' },
+            { role: 'front' },
+            { type: 'separator' },
+            { role: 'window' }
+          ]
+        : [
+            { role: 'close' }
+          ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
